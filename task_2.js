5c9013eea2d0af874302d54629f600266b0f5355
abdallah-04 <125401704+abdallah-04@users.noreply.github.com>
2025-02-23 00:48:20 +0300

    Update task_2.js

--- task_2.js
+++ task_2.js
@@ -1,9 +1,29 @@
-let studant=[]
-let sucstudant=[]
-let filstudant=[]
-function creatstudant(name,age,mark)
-{
+let students = [];
+let sucstudents = []; 
+let filstudents = [];
 
-    return studant.push({name: "Lina", age: 21, mark: 88 })
+function createstudent(name, age, mark) 
+{
+    students.push({ name: name, age: age, mark: mark });
 }
-console.log(studant)
+
+createstudent("Abdallah", 20, 99);
+createstudent("Khattab 3m aljame3", 20, 100);
+createstudent("Abdallah 3mk :)", 20, 98);
+createstudent("Eren", 89, 30);
+createstudent("Ice Bear", 32, 30);
+
+students.forEach(stu => {
+    if (stu.mark >= 50) 
+    {
+        sucstudents.push(stu); 
+    }
+    else 
+    {
+        filstudents.push(stu); 
+    }
+});
+
+console.log("Success Students:", sucstudents);
+console.log("Failed Students:", filstudents);
+
